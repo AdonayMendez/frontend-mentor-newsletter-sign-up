8c364c7eadb8d012bb464ac74a2e65432166df19
Adonay Mendez <adonay.mendez255@gmail.com>
2026-07-03 23:59:50 -0400

    Add popup on submit click

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 const form = document.getElementById("form"); 
+const successMsgPopup = document.getElementById("successMsgPopup");
+const submit = document.getElementById("submit");
+const inputField = document.getElementById("email"); 
 
 
 function handleSubmit(e){
-  e.preventDefault()
-
+  e.preventDefault();
+  
   const data = {};
-  const fields = e.target.querySelectorAll("input:not([type='submit'])");
-
-  for (let field of fields){
-    data[field.name] = field.value;
-  }
-
+  const emailField = document.getElementById("email");
+  data[emailField.name] = emailField.value;
   console.log(data);
+
+  showSuccessScreen();
+}
+
+function showSuccessScreen(){
+  const isShown = successMsgPopup.classList.toggle("show"); 
+
+  submit.textContent = isShown ? "Dismiss Message" : "Submit";
 }
 
 form.addEventListener("submit", (e) => {
