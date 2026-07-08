492682f3da2fca99cc61d44cf2319934add3d898
Adonay Mendez <adonay.mendez255@gmail.com>
2026-07-08 00:06:48 -0400

    Add custom input field validation styling

--- script.js
+++ script.js
@@ -1,39 +1,59 @@
 const form = document.getElementById("form"); 
 const successMsgPopup = document.getElementById("successMsgPopup");
 const submit = document.getElementById("submit");
-const inputField = document.getElementById("email"); 
 const conformationMsg = document.getElementById("conformationMsg");
 const newsletterContainer = document.getElementById("newsletterContainer");
 const successMessagePopupContainer = document.getElementById("successMessagePopupContainer");
 const dismissBtn = document.getElementById("dismissBtn");
+const errorMessage = document.querySelector(".error-message"); 
 
 
 
 
 function handleSubmit(e){
   e.preventDefault();
-  
-  const data = {};
+
   const emailField = document.getElementById("email");
+
+
+
+  if(emailField.value === ""){
+    errorMessage.textContent = "Email is required";
+    errorMessage.classList.add("show"); 
+    emailField.classList.add("error");
+    return; 
+  }
+
+  
+  if(!emailField.validity.valid){
+    errorMessage.textContent = "Valid email required"
+    errorMessage.classList.add("show"); 
+    emailField.classList.add("error");
+    return; 
+  }
+
+  errorMessage.classList.remove("show");
+  emailField.classList.remove("error");
+
+
+  const data = {};
 
   // Adds a property with the name given in the name attribute in the input
   // Sets the property value equal to what the user entered 
   data[emailField.name] = emailField.value;
   console.log(data);
 
-  showSuccessScreen();
-
   conformationMsg.innerHTML = `
   A confirmation email has been sent to <span>${emailField.value}</span>.
   Please open it and click the button inside to confirm your subscription
   `;
+
+  showSuccessScreen();
 }
 
 function showSuccessScreen(){
   successMessagePopupContainer.classList.toggle("show");
   successMsgPopup.classList.add("show"); 
-  // submit.textContent = isShown ? "Dismiss Message" : "Submit";
-  // newsletterContainer.classList.toggle("hide");
 }
 
 function hideSuccessScreen(){
